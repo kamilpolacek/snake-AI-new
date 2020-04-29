8d6a298c8f888e4211cb4782eef9c4df48b39a16
Kamil Poláček <polackam@fit.cvut.cz>
2020-04-29 07:00:36 +0200

    hopefully game logic is finished

--- game.js
+++ game.js
@@ -2,41 +2,44 @@ const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 
 
-const size = 20;
-var speed = 150;
-var total = 0;
-var commands = 0;
-var time = 0;
-
+const size = 20; // size of snake pieces
+var speed = 150; // speed of the game
+var commands = 0; // counts commands from keyboard -> only one per interval allowed
+var time = 0; // for ending the setInterval
+var start = false; // true if the game started
 
 const width = canvas.width = 600;
 const height = canvas.height = 600;
 
-
+/******** main function for the game ********/
 (function setup() {
   snake = new Snake();
-
-
+  //initial logic and game drawing
+  snake.move();
+  snake.move(); // dealing with snakes tail (twice)
+  snake.create();
   snake.pickFruitLocation();
+  snake.createFruit();
 
   time = window.setInterval(() => {
-    //console.log("hii");
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    if(snake.eatFruit()){
-      snake.pickFruitLocation();
+    if(start) {
+      ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+      if(snake.eatFruit()){
+        snake.pickFruitLocation();
+      }
+
+      snake.createFruit();
+      snake.move();
+      snake.create();
+      snake.checkCrash(time);
     }
-    snake.createFruit();
-    snake.move();
-    snake.create();
-    //did snake hit something?
-    snake.checkCrash(time);
-    //did snake had some apple?
-    //console.log("hi");
   }, speed);
 }());
-
+/******** listens for keyboard input ********/
 window.addEventListener('keydown', ((evt) => {
   var direction = evt.key.replace('Arrow', '');
+  start = true;
 
   if(commands == 0) {
     snake.changeDirection(direction);
--- snake.js
+++ snake.js
@@ -3,31 +3,31 @@ function Snake() {
   this.y = canvas.height / 2; //initial snake y position
   this.fruitX = 0; // fruit x position
   this.fruitY = 0; // fruit y position
-
-  this.speedX = 0; // direction of movement ( right(+) / left(-) )
-  this.speedY = 0; // direction of movement ( up(-) / down(+) )
+  this.speedX = 0; // direction of the movement ( right(+20px) / left(-20) )
+  this.speedY = 0; // direction of the movement ( up(-20) / down(+20) )
   this.tail = []; // array for tail of the snake
-  this.total = 0; // total pieces of tail
+  this.total = 3; // total pieces of tail
 
-
+/******** draws snake on the screen ********/
   this.create = function() {
-    ctx.fillStyle = "yellow";
     ctx.font = "20px Georgia";
-    ctx.fillText("Score : " + this.total, 500, 30);
+    ctx.fillStyle = "red";
+    ctx.fillText("Score : " + (this.total-3), 500, 30);
+
 
     for(let i=0; i<this.tail.length; i++) {
+      ctx.fillStyle = "blue";
       ctx.fillRect(this.tail[i].x,this.tail[i].y, size, size);
     }
 
+    ctx.fillStyle = "yellow";
     ctx.fillRect(this.x, this.y, size, size);
     commands = 0;
   }
-
+/******** move snakes in logic before drawning it ********/
   this.move = function() {
-    this.tail[this.total - 1] = { x: this.x, y: this.y };
-    this.tail.forEach(function(item, index, array) {
-      //console.log(item, index);
-    });
+    this.tail[this.total - 1] = { x: this.x, y: this.y };  //adding new piece of snake
+
     for (let i=0; i<this.tail.length - 1; i++) {
       this.tail[i] = this.tail[i+1];
     }
@@ -35,28 +35,28 @@ function Snake() {
     this.x += this.speedX;
     this.y += this.speedY;
   }
-  this.pickFruitLocation = function() {
-    this.fruitX = Math.ceil(Math.random() * (width / size)-1) * size;
-    this.fruitY = Math.ceil(Math.random() * (height / size)-1) * size;
-
-    if(this.fruitX == this.x && this.fruitY == this.y) {
-      console.log("picking again 1");
-      this.pickFruitLocation();
-    }
-
-    for(let i=0; i<this.tail.length; i++) {
-      if(this.fruitX == this.tail[i].x && this.fruitY == this.tail[i].y) {
-        console.log("picking again 2");
-        this.pickFruitLocation();
-      }
-    }
-  }
-
+/******** draws fruit on the screen ********/
   this.createFruit = function() {
     ctx.fillStyle = "blue";
     ctx.fillRect(this.fruitX, this.fruitY, size, size)
   }
 
+/******** pick location for fruit avoiding snake ********/
+  this.pickFruitLocation = function() {
+    this.fruitX = Math.ceil(Math.random() * (width / size)-1) * size;
+    this.fruitY = Math.ceil(Math.random() * (height / size)-1) * size;
+
+    if(this.fruitX == this.x && this.fruitY == this.y) {
+      this.pickFruitLocation();
+    }
+
+    for(let i=0; i<this.tail.length; i++) {
+      if(this.fruitX == this.tail[i].x && this.fruitY == this.tail[i].y) {
+        this.pickFruitLocation();
+      }
+    }
+  }
+/******** changing direction of movement ********/
   this.changeDirection = function(direction) {
     if(this.wrongDirection(direction)) {
       direction = "ignore :)";
@@ -80,9 +80,8 @@ function Snake() {
         this.speedY = 0;
         break;
     }
-
   }
-
+/******** checking if suggested direction can be moved to ********/
   this.wrongDirection = function(direction) {
     if(direction == 'Up' && this.speedY == size && this.speedX == 0) {
       return true;
@@ -90,24 +89,27 @@ function Snake() {
     if(direction == 'Right' && this.speedY == 0 && this.speedX == -size) {
       return true;
     }
+
     if(direction == 'Down' && this.speedY == -size && this.speedX == 0) {
       return true;
     }
+
     if(direction == 'Left' && this.speedY == 0 && this.speedX == size) {
       return true;
     }
 
     return false;
   }
-
+/******** same location as fruit? = eat it ********/
   this.eatFruit = function() {
     if(this.x == this.fruitX && this.y == this.fruitY) {
       this.total++;
       return true;
     }
+
     return false;
   }
-
+/******** checking for crashing ********/
   this.checkCrash = function() {
     for (let i=0; i<this.tail.length; i++) {
       if (this.tail[i].x == this.x  && this.tail[i].y == this.y)  {
@@ -118,16 +120,16 @@ function Snake() {
     if (this.x < 0 || this.x == canvas.width) {
       this.gameOver();
     }
+
     if (this.y < 0 || this.y == canvas.height) {
       this.gameOver();
     }
   }
-
+/******** ending the game ********/
   this.gameOver = function() {
     document.write("GAME OVER!");
     window.clearInterval(time);
   }
-
 
 
 }
